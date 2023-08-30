5e1f3888272321d366aefd799d27a4d7b7f8c28e
aswathyeg <aswathy.eg.bijesh@gmail.com>
2023-08-30 14:35:19 +0530

    image updated

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,31 @@
 //domain.com/
 import MeetupList from "@/components/meetups/MeetupList";
-import city1 from "../components/assets/city1.jpg";
+import Layout from "@/components/layout/Layout";
+import city2 from "@/components/assets/city2.jpg";
 function HomePage() {
   const DUMMY_DATA = [
     {
       id: "m1",
       title: "first",
-      image: { city1 },
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg",
       address: "address1",
       description: "first meetup",
     },
+    {
+      id: "m2",
+      title: "second",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg",
+      address: "address2",
+      description: "second meetup",
+    },
   ];
-  return <MeetupList meetups={DUMMY_DATA} />;
+  return (
+    <Layout>
+      {" "}
+      <MeetupList meetups={DUMMY_DATA} />
+    </Layout>
+  );
 }
 export default HomePage;
