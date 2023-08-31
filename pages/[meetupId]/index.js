1630ecbc317e10e84691504a35517fd6022c3757
aswathyeg <aswathy.eg.bijesh@gmail.com>
2023-08-31 16:34:30 +0530

    added detail

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,12 @@
+import MeetupDetail from "@/components/meetups/MeetupDetail";
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg"
+      title="first"
+      address="street 2,cross3,some city"
+      description="About meetup"
+    />
+  );
+}
+export default MeetupDetails;
