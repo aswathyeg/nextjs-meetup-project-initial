829d11e16185033dcf0b823b5b41665948f6d856
aswathyeg <aswathy.eg.bijesh@gmail.com>
2023-08-31 16:46:53 +0530

    updated

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,25 @@
 //domain.com/
 import MeetupList from "@/components/meetups/MeetupList";
 
-import city2 from "@/components/assets/city2.jpg";
+const DUMMY_DATA = [
+  {
+    id: "m1",
+    title: "first",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg",
+    address: "address1",
+    description: "first meetup",
+  },
+  {
+    id: "m2",
+    title: "second",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg",
+    address: "address2",
+    description: "second meetup",
+  },
+];
 function HomePage() {
-  const DUMMY_DATA = [
-    {
-      id: "m1",
-      title: "first",
-      image:
-        "https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg",
-      address: "address1",
-      description: "first meetup",
-    },
-    {
-      id: "m2",
-      title: "second",
-      image:
-        "https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg",
-      address: "address2",
-      description: "second meetup",
-    },
-  ];
   return <MeetupList meetups={DUMMY_DATA} />;
 }
 export default HomePage;
