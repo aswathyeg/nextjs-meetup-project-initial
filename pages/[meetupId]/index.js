e4abb8b88686bba4407c8ff79045140ed52dd900
aswathyeg <aswathy.eg.bijesh@gmail.com>
2023-09-07 20:13:29 +0530

    adde functions to fetch data

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -9,4 +9,37 @@ function MeetupDetails() {
     />
   );
 }
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+  };
+}
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+  console.log(meetupId);
+
+  return {
+    props: {
+      meetupData: {
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg",
+        title: "first",
+        address: "street 2,cross3,some city",
+        description: "About meetup",
+      },
+    },
+  };
+}
 export default MeetupDetails;
