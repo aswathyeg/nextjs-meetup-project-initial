c3c3574d6d7d01851f743762e774714dd03da200
aswathyeg <aswathy.eg.bijesh@gmail.com>
2023-09-08 15:01:57 +0530

    added connection

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,6 +1,18 @@
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
   if (req.method == "POST") {
     const data = req.body;
-    const { titile, image, address, description } = data;
+    // const { titile, image, address, description } = data;
+    const client = await MongoClient.connect(
+      "mongodb+srv://aswathy:mongoDBAswathy@cluster0.twrg0fy.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+    const meetupsCollection = db.collection("meetups");
+    const result = await meetupsCollection.insertOne(data);
+    console.log(result);
+    client.close();
+    res.status(201).json({ message: "Meetup inserted !" });
   }
 }
+export default handler;
