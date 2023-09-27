7fb0b5bba956ce82269c08cbf023de8558f91c4a
aswathyeg <aswathy.eg.bijesh@gmail.com>
2023-09-27 16:32:25 +0530

    added outer card

--- components/meetups/MeetupItem.js
+++ components/meetups/MeetupItem.js
@@ -1,4 +1,5 @@
 import Card from "../ui/Card";
+import OuterCard from "../ui/OuterCard";
 import classes from "./MeetupItem.module.css";
 import { useRouter } from "next/router";
 
@@ -9,18 +10,20 @@ function MeetupItem(props) {
   }
   return (
     <li className={classes.item}>
-      <Card>
-        <div className={classes.image}>
-          <img src={props.image} alt={props.title} />
-        </div>
-        <div className={classes.content}>
-          <h3>{props.title}</h3>
-          <address>{props.address}</address>
-        </div>
-        <div className={classes.actions}>
-          <button onClick={handleClick}>Show Details</button>
-        </div>
-      </Card>
+      <OuterCard>
+        <Card>
+          <div className={classes.image}>
+            <img src={props.image} alt={props.title} />
+          </div>
+          <div className={classes.content}>
+            <h3>{props.title}</h3>
+            <address>{props.address}</address>
+          </div>
+          <div className={classes.actions}>
+            <button onClick={handleClick}>Show Details</button>
+          </div>
+        </Card>
+      </OuterCard>
     </li>
   );
 }
